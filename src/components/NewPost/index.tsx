c77154da660586995fdfcbe8197702755907236c
debfdias <debfdias@gmail.com>
2023-03-02 21:31:50 -0300

    Form styles

--- src/components/NewPost/index.tsx
+++ src/components/NewPost/index.tsx
@@ -1,13 +1,28 @@
+import styles from "./NewPost.module.css";
+
 interface NewPostProps {
   onSubmit: () => void;
 }
 
 export function NewPost({ onSubmit }: any) {
   return (
-    <form onSubmit={onSubmit}>
-      <textarea rows={3} name="content" />
-      <input name="author" />
-      <button type="submit">Submit</button>
+    <form onSubmit={onSubmit} className={styles.formBox}>
+      <textarea
+        className={styles.contentBox}
+        rows={5}
+        name="content"
+        placeholder="Type something here!"
+      />
+      <div className={styles.row}>
+        <input
+          className={styles.authorBox}
+          name="author"
+          placeholder="Author name"
+        />
+        <button className={styles.submitButton} type="submit">
+          Create
+        </button>
+      </div>
     </form>
   );
 }
